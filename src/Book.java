f6056b65f7f58f358cd60ad627254715cb071994
Lubava <Lwm28@rambler.ru>
2022-11-28 00:48:58 +0300

    ДЗ 12. Методы объектов

---
+++ src/Book.java
@@ -0,0 +1,59 @@
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private Author author;
+    private int yearPublished;
+
+    public Book(String title, Author author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Book" +
+                " " + title + " "+
+                 "Author" +
+                ", yearPubleshed " + yearPublished;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublished == book.yearPublished && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, yearPublished);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Ivan", "Turgenev");
+        Author author2 = new Author("Lev", "Tolstoy");
+        Book book1 = new Book("Novy", author1, 1876);
+        Book book2 = new Book("Voyna i mir", author2, 1867);
+        Book book3 = new Book("Ozma of Oz", new Author("L.Frank", "Baum"), 1907);
+        //System.out.println(book1);
+        //System.out.println(book2);
+        //System.out.println(book3);
+        Book[] library = new Book[3];
+        library[0] = book1;
+        library[1] = book2;
+        library[2] = book3;
+        for (Book book : library) {
+            System.out.println(book);
+
+        }
+
     }
 }
